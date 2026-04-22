9f3db7bd71fe0b9e19323acecc3a668986453027
Zhengyang Lu <stevenlu176@gmail.com>
2026-04-22 16:24:16 +0000

    Add at_goal variable and create transit and transport actions.

--- modular_construction_task_planner/modular_construction_task_planner/scripts/block_domain.py
+++ modular_construction_task_planner/modular_construction_task_planner/scripts/block_domain.py
@@ -54,6 +54,10 @@ class Holding(Variable):
     domain: str = 'block'
 
 @dataclass
+class AtGoal(Variable):
+    domain: str = 'bool'
+
+@dataclass
 class RobotAt(Variable):
     domain: str = 'robo_pos'
 
@@ -65,6 +69,8 @@ class Object(Entity):
     below: BelowBlock = field(default_factory=BelowBlock)
     supported: Supported = field(default_factory=Supported)
     goal: Goal = field(default_factory=Goal)
+    reachable_from: List[str] = field(default_factory=list)
+    placeable_from: List[str] = field(default_factory=list)
 
     def __post_init__(self):
         self.at_top.value = True
@@ -85,24 +91,32 @@ class Robot(Entity):
     at: RobotAt = field(default_factory=RobotAt)
     gripper_empty: GripperEmpty = field(default_factory=GripperEmpty)
     holding: Holding = field(default_factory=Holding)
+    at_goal: AtGoal = field(default_factory=AtGoal)
 
     def __post_init__(self):
         self.gripper_empty.value = True
         self.holding.value = None
+        self.at_goal.value = False
 
 # Action definitions
-move_parameters = {
+transit_parameters = {
     'robot': Robot,
     'start_pose': PosEntity,
     'target_pose': PosEntity
 }
-move_conditions = [
-    Condition('robot_at_start', 'robot', 'at', 'start_pose')
+transit_conditions = [
+    Condition('robot_at_start', 'robot', 'at', 'start_pose'),
+    Condition('robot_not_at_goal', 'robot', 'at_goal', False),
+    Condition('target_pose_clear', 'target_pose', 'clear', True),
+    # Condition('target_pose_reachable', 'target_object', 'reachable_from', 'target_pose') This needs to be different,
+    # it should be a computed predicate since we need to check if target_pose is in target_object.reachable_from
 ]
-move_effects = [
-    Effect('move_robot_to_target', 'robot', 'at', 'target_pose')
+transit_effects = [
+    Effect('move_robot_to_target', 'robot', 'at', 'target_pose'),
+    Effect('robot_not_at_start', 'start_pose', 'occupied_by', None),
+    Effect('target_pose_occupied_by_robot', 'target_pose', 'occupied_by', 'robot'),
+    Effect('target_pose_not_clear', 'target_pose', 'clear', False),
 ]
-MoveAction = Action('move', move_parameters, move_conditions, move_effects)
 
 pick_parameters = {
     'robot': Robot,
@@ -126,6 +140,26 @@ pick_effects = [
 ]
 PickAction = Action('pick', pick_parameters, pick_conditions, pick_effects)
 
+transport_parameters = {
+    'robot': Robot,
+    'start_pos': PosEntity,
+    'target_pose': PosEntity
+}
+transport_conditions = [
+    Condition('robot_at_start', 'robot', 'at', 'start_pos'),
+    Condition('robot_not_at_goal', 'robot', 'at_goal', False),
+    Condition('gripper_not_empty', 'robot', 'gripper_empty', False),
+    Condition('holding_object', 'robot', 'holding', 'object'),
+    Condition('target_pos_clear', 'target_pose', 'clear', True)
+]
+transport_effects = [
+    Effect('move_robot_to_goal', 'robot', 'at', 'target_pose'),
+    Effect('target_pose_occupied_by_robot', 'target_pose', 'occupied_by', 'robot'),
+    Effect('target_pose_not_clear', 'target_pose', 'clear', False),
+    Effect('robot_not_at_start', 'start_pos', 'occupied_by', None),
+    Effect('robot_at_goal', 'robot', 'at_goal', True),
+]
+
 place_parameters = {
     'robot': Robot,
     'object': Object,
@@ -143,5 +177,19 @@ place_effects = [
     Effect('object_at_target', 'object', 'at', 'target_pose'),
     Effect('target_pose_occupied_by_object', 'target_pose', 'occupied_by', 'object'),
     Effect('target_pose_not_clear', 'target_pose', 'clear', False),
+    Effect('robot_not_at_goal', 'robot', 'at_goal', False),
 ]
 PlaceAction = Action('place', place_parameters, place_conditions, place_effects)
+
+move_parameters = {
+    'robot': Robot,
+    'start_pose': PosEntity,
+    'target_pose': PosEntity
+}
+move_conditions = [
+    Condition('robot_at_start', 'robot', 'at', 'start_pose')
+]
+move_effects = [
+    Effect('move_robot_to_target', 'robot', 'at', 'target_pose')
+]
+MoveAction = Action('move', move_parameters, move_conditions, move_effects)
